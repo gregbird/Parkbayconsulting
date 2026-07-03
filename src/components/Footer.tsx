4ea6132fe55ebf3be3fc9a6be4b80efe2aabd386
All good <gbirdthistle@gmail.com>
2026-07-03 13:30:59 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,11 +83,11 @@ export default function Footer() {
             <ul className="flex flex-col gap-4">
               <li>
                 <a
-                  href="mailto:hello@parkbayconsulting.ie"
+                  href="mailto:ciara.mccluskey@gmail.com"
                   className="flex items-start gap-2.5 text-[14px] text-white/70 hover:text-white transition-colors duration-200"
                 >
                   <Mail size={15} className="text-gold flex-shrink-0 mt-[2px]" />
-                  hello@parkbayconsulting.ie
+                  hello@parkbayconsulting.com
                 </a>
               </li>
               <li>
